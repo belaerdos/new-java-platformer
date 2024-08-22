8c73a6248828e76155f50fc9a1890187180aa262
belaerdos <erdosbelamusic@gmail.com>
2024-08-22 08:19:08 +0200

    episode 2 inputs

--- platformer-project/src/inputs/KeyboardInputs.java
+++ platformer-project/src/inputs/KeyboardInputs.java
@@ -1,9 +1,16 @@
 package inputs;
 
+import main.GamePanel;
+
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class KeyboardInputs implements KeyListener {
+
+    private GamePanel gamePanel;
+    public KeyboardInputs(GamePanel gamePanel){
+        this.gamePanel = gamePanel;
+    }
     @Override
     public void keyTyped(KeyEvent e) {
 
@@ -17,16 +24,16 @@ public class KeyboardInputs implements KeyListener {
         switch(e.getKeyCode()){
 
             case KeyEvent.VK_W:
-                System.out.println("W");
+                gamePanel.changeYDelta(-5);
                 break;
             case KeyEvent.VK_A:
-                System.out.println("A");
+                gamePanel.changeXDelta(-5);
                 break;
             case KeyEvent.VK_S:
-                System.out.println("S");
+                gamePanel.changeYDelta(5);
                 break;
             case KeyEvent.VK_D:
-                System.out.println("D");
+                gamePanel.changeXDelta(5);
                 break;
         }
     }
