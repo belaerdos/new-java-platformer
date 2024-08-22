0f9fafe3cb2d51210daaf91b6507b23374ed8bbc
belaerdos <erdosbelamusic@gmail.com>
2024-08-22 07:48:30 +0200

    added keys and mouse

---
+++ platformer-project/src/inputs/KeyboardInputs.java
@@ -0,0 +1,34 @@
+package inputs;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyboardInputs implements KeyListener {
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+    @Override
+    public void keyPressed(KeyEvent e) {
+        switch(e.getKeyCode()){
+
+            case KeyEvent.VK_W:
+                System.out.println("W");
+                break;
+            case KeyEvent.VK_A:
+                System.out.println("A");
+                break;
+            case KeyEvent.VK_S:
+                System.out.println("S");
+                break;
+            case KeyEvent.VK_D:
+                System.out.println("D");
+                break;
+        }
+    }
+
+}
--- platformer-project/src/main/GamePanel.java
+++ platformer-project/src/main/GamePanel.java
@@ -1,12 +1,18 @@
 package main;
 
+import inputs.KeyboardInputs;
+import inputs.MouseInputs;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class GamePanel extends JPanel {
-
+    private MouseInputs mouseInputs;
     public GamePanel(){
-
+        mouseInputs = new MouseInputs();
+        addKeyListener(new KeyboardInputs());
+        addMouseListener(mouseInputs);
+        addMouseMotionListener(mouseInputs);
     }
 
     public void paintComponent(Graphics g){
